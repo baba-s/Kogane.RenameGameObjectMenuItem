c5851fefcfcb3f7b887b69e36b4f3f702e746b17
baba-s <shotababa1206@gmail.com>
2022-08-27 15:03:31 +0900

    add

---
+++ Editor/RenameGameObjectMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class RenameGameObjectMenuItem
+    {
+        [MenuItem( "CONTEXT/Component/Rename Game Object" )]
+        private static void Rename( MenuCommand menuCommand )
+        {
+            var type       = menuCommand.context.GetType();
+            var name       = type.Name;
+            var component  = ( Component )menuCommand.context;
+            var gameObject = component.gameObject;
+
+            Undo.RecordObject( gameObject, "Rename" );
+
+            gameObject.name = name;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.RenameGameObjectMenuItem
+# Kogane Rename Game Object Menu Item
+
+コンポーネントの名前をゲームオブジェクトの名前に適用する MenuItem を追加するエディタ拡張
